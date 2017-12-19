6df5ae343283bf24aa7e6ee4acd052fc8cac9ef7
18210930320@163.com <you@example.com>
2017-12-19 20:41:49 +0800

    专题增加了类别列表

--- app/src/main/java/com/happy/bwiesample/mvp/view/fragment/ZTFragment.java
+++ app/src/main/java/com/happy/bwiesample/mvp/view/fragment/ZTFragment.java
@@ -1,6 +1,7 @@
 package com.happy.bwiesample.mvp.view.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.happy.bwiesample.R;
 import com.happy.bwiesample.base.BaseMvpFragment;
@@ -16,6 +18,7 @@ import com.happy.bwiesample.helper.RetrofitHelper;
 import com.happy.bwiesample.mvp.presenter.JXPresenter;
 import com.happy.bwiesample.mvp.presenter.ZTPresenter;
 import com.happy.bwiesample.mvp.view.ZTView;
+import com.happy.bwiesample.mvp.view.activity.VideoListActivity;
 import com.happy.bwiesample.mvp.view.adapter.SpecialRecyclerAdapter;
 
 import java.util.List;
@@ -45,10 +48,19 @@ public class ZTFragment extends BaseMvpFragment<ZTPresenter> implements ZTView {
     }
 
     @Override
-    public void showZTList(List<VideoType> typeList) {
+    public void showZTList(final List<VideoType> typeList) {
         SpecialRecyclerAdapter adapter = new SpecialRecyclerAdapter(context,typeList);
         special_rv.setLayoutManager(new GridLayoutManager(context,2));
         special_rv.setAdapter(adapter);
+        adapter.setOnItemClick(new SpecialRecyclerAdapter.setOnItemClick() {
+            @Override
+            public void ItemCliek(View view, int position) {
+                Toast.makeText(context, ""+position, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(context, VideoListActivity.class);
+                intent.putExtra("url",typeList.get(position).moreURL);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
