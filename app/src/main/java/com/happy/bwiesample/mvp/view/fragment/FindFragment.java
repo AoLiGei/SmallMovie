615a4c18f492724dee8747831df9b8100cf2353f
tangyixin01 <496257944@qq.com>
2017-12-19 21:22:38 +0800

    综合页面

--- app/src/main/java/com/happy/bwiesample/mvp/view/fragment/FindFragment.java
+++ app/src/main/java/com/happy/bwiesample/mvp/view/fragment/FindFragment.java
@@ -1,5 +1,15 @@
 package com.happy.bwiesample.mvp.view.fragment;
 
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 import com.happy.bwiesample.R;
 import com.happy.bwiesample.base.BaseMvpFragment;
 import com.happy.bwiesample.entry.VideoHttpResponse;
