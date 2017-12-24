67f819a779f73631507538fe17ecc6b588c0d487
18210930320@163.com <you@example.com>
2017-12-24 19:35:47 +0800

    增加了ZTFragment的Progressbar,和主题的布局

--- app/src/main/java/com/happy/bwiesample/mvp/view/fragment/ZTFragment.java
+++ app/src/main/java/com/happy/bwiesample/mvp/view/fragment/ZTFragment.java
@@ -1,12 +1,17 @@
 package com.happy.bwiesample.mvp.view.fragment;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.happy.bwiesample.R;
@@ -36,12 +41,24 @@ import javax.inject.Inject;
  */
 
 public class ZTFragment extends BaseMvpFragment<ZTPresenter> implements ZTView {
+
     @Inject
     Context context;
     private RecyclerView recyclerView;
     private SpringView sv;
     private List<VideoInfo> videoInfos;
+    private ProgressBar progressBar;
+    private TextView tv_error;
+    private SpecialRecyclerAdapter adapter;
 
+
+    //    Handler handler = new Handler(){
+//        @Override
+//        public void handleMessage(Message msg) {
+//            super.handleMessage(msg);
+//            progressBar.setProgress();
+//        }
+//    };
     @Override
     public int setLayout() {
         return R.layout.fragment_zt;
@@ -58,8 +75,9 @@ public class ZTFragment extends BaseMvpFragment<ZTPresenter> implements ZTView {
         super.initView();
         recyclerView = view.findViewById(R.id.special_rv);
         sv = view.findViewById(R.id.special_sp);
-
-
+        progressBar = view.findViewById(R.id.special_pb);
+//        tv_error = view.findViewById(R.id.special_tv);
+//        progressBar.setMax(100);
     }
 
     @Override
@@ -70,41 +88,46 @@ public class ZTFragment extends BaseMvpFragment<ZTPresenter> implements ZTView {
 
     @Override
     public void showZTList(final VideoHttpResponse<VideoRes> videoResVideoHttpResponse) {
-        VideoRes videoRes = videoResVideoHttpResponse.getRet();
-        videoInfos = new ArrayList<>();
-        for (int i=1;i<videoRes.list.size();i++){
-            if (!TextUtils.isEmpty(videoRes.list.get(i).moreURL) && !TextUtils.isEmpty(videoRes.list.get(i).title)) {
-                VideoInfo videoInfo = videoRes.list.get(i).childList.get(0);
-                videoInfo.title = videoRes.list.get(i).title;
-                videoInfo.moreURL = videoRes.list.get(i).moreURL;
-                videoInfos.add(videoInfo);
+        if (videoResVideoHttpResponse != null) {
+            progressBar.setVisibility(View.GONE);
+            recyclerView.setVisibility(View.VISIBLE);
+            VideoRes videoRes = videoResVideoHttpResponse.getRet();
+            videoInfos = new ArrayList<>();
+            for (int i = 1; i < videoRes.list.size(); i++) {
+                if (!TextUtils.isEmpty(videoRes.list.get(i).moreURL) && !TextUtils.isEmpty(videoRes.list.get(i).title)) {
+                    VideoInfo videoInfo = videoRes.list.get(i).childList.get(0);
+                    videoInfo.title = videoRes.list.get(i).title;
+                    videoInfo.moreURL = videoRes.list.get(i).moreURL;
+                    videoInfos.add(videoInfo);
+                }
             }
+            recyclerView.setLayoutManager(new GridLayoutManager(context, 2));
+            adapter = new SpecialRecyclerAdapter(context, videoInfos);
+            recyclerView.setAdapter(adapter);
+            adapter.setOnItemClick(new SpecialRecyclerAdapter.setOnItemClick() {
+                @Override
+                public void ItemCliek(View view, int position) {
+                    Intent intent = new Intent(context, VideoListActivity.class);
+                    intent.putExtra("url", videoInfos.get(position).moreURL);
+                    intent.putExtra("name", videoInfos.get(position).title);
+                    startActivity(intent);
+                }
+            });
+
+            sv.setListener(new SpringView.OnFreshListener() {
+                @Override
+                public void onRefresh() {
+                    p.getCommit();
+                    adapter.upDate(videoInfos);
+                    sv.onFinishFreshAndLoad();
+                }
+
+                @Override
+                public void onLoadmore() {
+                }
+            });
+
         }
-        recyclerView.setLayoutManager(new GridLayoutManager(context, 2));
-        final SpecialRecyclerAdapter adapter = new SpecialRecyclerAdapter(context, videoInfos);
-        recyclerView.setAdapter(adapter);
-        adapter.setOnItemClick(new SpecialRecyclerAdapter.setOnItemClick() {
-            @Override
-            public void ItemCliek(View view, int position) {
-                Intent intent = new Intent(context, VideoListActivity.class);
-                intent.putExtra("url",videoInfos.get(position).moreURL);
-                intent.putExtra("name", videoInfos.get(position).title);
-                startActivity(intent);
-            }
-        });
-
-        sv.setListener(new SpringView.OnFreshListener() {
-            @Override
-            public void onRefresh() {
-                p.getCommit();
-                adapter.upDate(videoInfos);
-                sv.onFinishFreshAndLoad();
-            }
-
-            @Override
-            public void onLoadmore() {
-            }
-        });
-
     }
+
 }
