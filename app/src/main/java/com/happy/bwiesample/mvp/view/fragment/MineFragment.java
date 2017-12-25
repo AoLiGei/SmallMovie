303f88652a58488a7cb67418916afab924ceb366
18210930320@163.com <you@example.com>
2017-12-25 10:15:29 +0800

    增加了主题的布局

--- app/src/main/java/com/happy/bwiesample/mvp/view/fragment/MineFragment.java
+++ app/src/main/java/com/happy/bwiesample/mvp/view/fragment/MineFragment.java
@@ -9,6 +9,7 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
@@ -21,6 +22,9 @@ import com.happy.bwiesample.base.BaseMvpFragment;
 import com.happy.bwiesample.mvp.presenter.JXPresenter;
 import com.happy.bwiesample.mvp.view.MineView;
 import com.happy.bwiesample.mvp.view.adapter.PopGvAdapter;
+import com.happy.bwiesample.wigdet.TheamCircleView;
+
+import org.w3c.dom.Text;
 
 import javax.inject.Inject;
 
@@ -37,6 +41,7 @@ public class MineFragment extends BaseMvpFragment<JXPresenter> implements MineVi
     private TextView tv_them;
     private RelativeLayout rl_them;
     private RelativeLayout relativeLayout;
+    private PopupWindow popupWindow;
 
     @Override
     public int setLayout() {
@@ -56,19 +61,30 @@ public class MineFragment extends BaseMvpFragment<JXPresenter> implements MineVi
         relativeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PopupWindow popupWindow = new PopupWindow(getContext());
+                popupWindow = new PopupWindow(getContext());
                 popupWindow.setWidth(ViewGroup.LayoutParams.WRAP_CONTENT);
                 popupWindow.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
-                View layout = LayoutInflater.from(getContext()).inflate(R.layout.layout_popupwindow,null);
-                GridView gridView= layout.findViewById(R.id.popwindow_gv);
-                gridView.setAdapter(new PopGvAdapter(getContext()));
+                View layout = LayoutInflater.from(getContext()).inflate(R.layout.layout_popupwindow, null);
+                GridView gridView = layout.findViewById(R.id.popwindow_gv);
+                TextView tv_dismiss = layout.findViewById(R.id.tv_dismiss);
+                TextView tv_click = layout.findViewById(R.id.tv_click);
+                tv_dismiss.setOnClickListener(this);
+                tv_click.setOnClickListener(this);
+                PopGvAdapter popGvAdapter = new PopGvAdapter(getContext());
+                gridView.setAdapter(popGvAdapter);
+                gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Toast.makeText(getContext(), "应用了主题" + position, Toast.LENGTH_SHORT).show();
+                        popupWindow.dismiss();
+                    }
+                });
+
                 popupWindow.setContentView(layout);
                 popupWindow.setBackgroundDrawable(new ColorDrawable(0xffffffff));
                 popupWindow.setOutsideTouchable(false);
                 popupWindow.setFocusable(true);
-                popupWindow.showAtLocation(relativeLayout, Gravity.CENTER,0,0);
-
-
+                popupWindow.showAtLocation(relativeLayout, Gravity.CENTER, 0, 0);
 
             }
         });
@@ -78,5 +94,6 @@ public class MineFragment extends BaseMvpFragment<JXPresenter> implements MineVi
     public void initData() {
         super.initData();
     }
+
 
 }
