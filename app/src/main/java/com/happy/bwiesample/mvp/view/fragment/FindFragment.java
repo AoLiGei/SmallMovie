df1da3a12593fa5a669590fc94f505754ff10198
²̻ªîì <685>
2017-12-25 10:42:15 +0800

    拉取

--- app/src/main/java/com/happy/bwiesample/mvp/view/fragment/FindFragment.java
+++ app/src/main/java/com/happy/bwiesample/mvp/view/fragment/FindFragment.java
@@ -1,5 +1,6 @@
 package com.happy.bwiesample.mvp.view.fragment;
 
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
@@ -15,8 +16,8 @@ import com.happy.bwiesample.entry.VideoRes;
 import com.happy.bwiesample.entry.VideoType;
 import com.happy.bwiesample.mvp.presenter.FindPresenter;
 import com.happy.bwiesample.mvp.view.FindView;
+import com.happy.bwiesample.mvp.view.activity.VideoPlayActivity;
 import com.happy.bwiesample.mvp.view.adapter.FindAdapter;
-import com.happy.bwiesample.wigdet.OverLayCardLayoutManager;
 import com.happy.bwiesample.wigdet.RenRenCallback;
 
 import java.util.List;
